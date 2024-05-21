ec0a4ae1b85b1b7864a7fe906fd13ec498b989b5
sryang <sarang628@gmail.com>
2024-05-21 16:19:50 +0800

    add search bar

--- app/src/main/java/com/sryang/findinglinkmodules/MainActivity.kt
+++ app/src/main/java/com/sryang/findinglinkmodules/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,13 +12,10 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.screen_map.compose.MapScreen
 import com.example.screen_map.compose.MapScreenForRestaurant
 import com.example.screen_map.data.MarkerData
-import com.google.maps.android.compose.rememberCameraPositionState
 import com.sryang.findinglinkmodules.di.finding_di.Finding
 import com.sryang.findinglinkmodules.ui.theme.FindingLinkModulesTheme
-import com.sryang.screen_filter.ui.FilterScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
